a93913b9e4c0bedaf2ad46b24040174608d50349
Pedro Fernandes <pedrovosvisk10@gmail.com>
2024-03-05 17:05:08 -0300

    Update Array.java

--- VetoresMatrizes/src/Array.java
+++ VetoresMatrizes/src/Array.java
@@ -4,10 +4,7 @@ public class Array {
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
-		double[] notas; // Declara uma variável do tipo vetor de "doubles"
-		notas = new double[20]; // Aloca memória para 20 elementos no vetor
 		
-//		int[] pesos = {40, 20 , 30, 10}; - Declara e inicializa um vetor de quatro posições
 	
 		Scanner sc = new Scanner(System.in);
 		
@@ -16,9 +13,7 @@ public class Array {
 			notas[i] = sc.nextDouble();
 		}
 		
-		for(double nota: notas) { // Associa uma variável a cata um dos elementos do array
 			System.out.printf("%.1f\n", nota);  
-		} // Não permite que o conteúdo do array seja modificado
 	
 	sc.close();
 	}
